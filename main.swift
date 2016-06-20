ced3094e1cc332416ce93aebf141dba8aee64d74
heiheiha <1137029429@qq.com>
2016-06-20 11:39:14 +0800

    swift practice
    看了网页上swift基础教学的视频，练习了一下如何创建变量，常量以及他们的类型，字符串的创建和连接，字典的创建和添加。

---
+++ main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  swift pratice
+//
+//  Created by  黄磊 on 16/6/20.
+//  Copyright © 2016年  黄磊. All rights reserved.
+//
+
+import Foundation
+var str = "hello"
+var i:Int=100
+var s:String="world"
+var words:String="world"
+str = str + "lessom"
+str = "\(str),huanglei,\(i)"
+var arr = ["hello world","huanglei","haha"]
+var dict = ["name":"huanglei","sex":"boy"]
+dict["age"] = "18"
+print("Hello, World!")
